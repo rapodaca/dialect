758db0f71a99b1a2afda6f6599f5ae36c8a3183f
Richard Apodaca <rich.apodaca@gmail.com>
2022-02-25 16:53:22 -0800

    Eliminates non_zero, sets lower bound of numerical values to zero. closes #60

--- src/railroad/digit.js
+++ src/railroad/digit.js
@@ -1,9 +1,17 @@
-const {Diagram, NonTerminal, Terminal, Choice } = require('../../lib/railroad-diagrams');
+const {Diagram, Terminal, Choice } = require('../../lib/railroad-diagrams');
 
 module.exports = Diagram(
   Choice(
     0,
     Terminal('0'),
-    NonTerminal('non_zero')
+    Terminal('1'),
+    Terminal('2'),
+    Terminal('3'),
+    Terminal('4'),
+    Terminal('5'),
+    Terminal('6'),
+    Terminal('7'),
+    Terminal('8'),
+    Terminal('9')
   )
 );
